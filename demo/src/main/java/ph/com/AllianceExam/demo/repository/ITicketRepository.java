d7962dc6d9762bdee387470ec2fe0a528b745919
Kent <jonathankentmorales@gmail.com>
2022-10-22 09:48:33 +0800

    Refactor Code (Tabs)

--- demo/src/main/java/ph/com/AllianceExam/demo/repository/ITicketRepository.java
+++ demo/src/main/java/ph/com/AllianceExam/demo/repository/ITicketRepository.java
@@ -7,7 +7,7 @@ import ph.com.AllianceExam.demo.entity.Ticket;
 public interface ITicketRepository {
 	public Ticket retreiveTicketByID(final int id);
 	public List<Ticket> retreiveAllTickets();
-  public int create(final Ticket ticket);
-  public int deleteByID(final int id);
-  public int updateByID(final Ticket ticket);
+	public int create(final Ticket ticket);
+	public int deleteByID(final int id);
+	public int updateByID(final Ticket ticket);
 }
--- demo/src/main/java/ph/com/AllianceExam/demo/service/ITicketService.java
+++ demo/src/main/java/ph/com/AllianceExam/demo/service/ITicketService.java
@@ -6,6 +6,6 @@ public interface ITicketService {
 	public int create(final BufferedReader body);
 	public String retreiveByID(final int id);
 	public String retreiveAllTickets();
-  public int deleteByID(final int id);
-  public int updateByID(final BufferedReader body);
+	public int deleteByID(final int id);
+	public int updateByID(final BufferedReader body);
 }
