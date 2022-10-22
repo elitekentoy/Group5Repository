c9d927ba0edec6bc3638f6c0e7e2ab1d9fb60fd9
Dandielle Fate Monter <dandielle.monter.20@usjr.edu.ph>
2022-10-22 11:06:45 +0800

    Update IService Parameter

--- demo/src/main/java/ph/com/AllianceExam/demo/service/ITicketService.java
+++ demo/src/main/java/ph/com/AllianceExam/demo/service/ITicketService.java
@@ -2,10 +2,12 @@ package ph.com.AllianceExam.demo.service;
 
 import java.io.BufferedReader;
 
+import ph.com.AllianceExam.demo.entity.Ticket;
+
 public interface ITicketService {
 	public int create(final BufferedReader body);
 	public String retreiveByID(final int id);
 	public String retreiveAllTickets();
 	public int deleteByID(final int id);
-	public int updateByID(final BufferedReader body);
+	public int updateByID(final Ticket ticket);
 }
